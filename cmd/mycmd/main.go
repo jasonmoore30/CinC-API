9b2a2fe1d6c7132e52a482eb2f8782ba8b015263
Jason Daniel Moore <jason.moore2@furman.edu>
2017-11-06 16:21:28 -0500

    DB nil pointer fix

--- cmd/mycmd/main.go
+++ cmd/mycmd/main.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 
 	cinc "github.com/jasonmoore30/CinC-API"
+	"github.com/jasonmoore30/CinC-API/models"
 )
 
 func main() {
 	cincObj, err := cinc.NewCinc()
-	// config := &cinc.DBConfig{
-	// 	Connection: "tcp(156.143.17.176)",
-	// 	DBName:     "jamoore",
-	// 	DBUser:     "jamoore",
-	// 	DBPass:     ".reset.",
-	// }
-	// dsn := config.DBUser + ":" + config.DBPass + "@" + config.Connection + "/" + config.DBName
-	// models.InitDB(dsn)
+	config := &cinc.DBConfig{
+		Connection: "tcp(156.143.17.176)",
+		DBName:     "jamoore",
+		DBUser:     "jamoore",
+		DBPass:     ".reset.",
+	}
+	dsn := config.DBUser + ":" + config.DBPass + "@" + config.Connection + "/" + config.DBName
+	models.InitDB(dsn)
 	if err != nil {
 		fmt.Println(err)
 	}
