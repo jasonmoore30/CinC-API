5aad22cc3e09fa0f173aa0fd8e240a53dd451df2
Jason Daniel Moore <jason.moore2@furman.edu>
2017-12-09 23:05:45 -0500

    config environment variables

--- cmd/CinC-API/main.go
+++ cmd/CinC-API/main.go
@@ -2,20 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"os"
 
 	cinc "github.com/jasonmoore30/CinC-API"
 	"github.com/jasonmoore30/CinC-API/models"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	cincObj, err := cinc.NewCinc()
-	config := &cinc.DBConfig{
-		Connection: "tcp(156.143.17.176)",
-		DBName:     "jcovington",
-		DBUser:     "jcovington",
-		DBPass:     "Z48tuaOs",
+	if err != nil {
+		log.Println(err)
+		return
 	}
-	dsn := config.DBUser + ":" + config.DBPass + "@" + config.Connection + "/" + config.DBName
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	env := os.Getenv("ENVIRONMENT")
+	var dsn string
+	if env == "dev" {
+		config := &cinc.DBConfig{
+			Connection: "tcp(156.143.17.176)",
+			DBName:     "jcovington",
+			DBUser:     "jcovington",
+			DBPass:     "Z48tuaOs",
+		}
+		dsn = config.DBUser + ":" + config.DBPass + "@" + config.Connection + "/" + config.DBName
+	} else {
+		config := &cinc.DBConfig{
+			Connection: "erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+			DBName:     "ekrazwe0spgirfvb",
+			DBUser:     "lg4zljacvp2tkm4x",
+			DBPass:     "clh3e6aww7a0600o",
+		}
+		dsn = config.DBUser + ":" + config.DBPass + "@" + config.Connection + "/" + config.DBName
+	}
+
+	//dsn := config.DBUser + ":" + config.DBPass + "@" + config.Connection + "/" + config.DBName
+	log.Println("DSN string being used: " + dsn)
 	models.InitDB(dsn)
 	if err != nil {
 		fmt.Println(err)
